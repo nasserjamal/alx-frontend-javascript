f2d18bc6392f1710392b020a720f8b26ce427f72
Nasser Jamal <nasserjamal30@gmail.com>
2023-06-29 15:09:07 +0300

    bug fixes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/no-unresolved
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
