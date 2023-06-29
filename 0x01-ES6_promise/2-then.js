ad40744b62c59221b6067cd3739520722d2ebf75
Nasser Jamal <nasserjamal30@gmail.com>
2023-06-29 12:37:28 +0300

    attempt 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -7,6 +7,7 @@ export default function handleResponseFromAPI(promise) {
         body: 'success',
       };
     },
+  ).catch(
     () => {
       console.log('Got a response from the API');
       return (new Error());
